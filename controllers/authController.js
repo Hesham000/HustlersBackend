4472ecd62539b845e57806eab5c8a3dad555fd6c
Hesham000 <92303209+Hesham000@users.noreply.github.com>
2024-09-11 02:22:43 +0400

    Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -84,7 +84,7 @@ exports.verifyOtp = async (req, res) => {
         const user = await User.findOne({ email });
 
         // Check if OTP is valid and not expired
-        const isValidOtp = verifyOtp(otp, user.verificationOtp); // Ensure you pass the correct arguments to verifyOtp
+        const isValidOtp = verifyOtp(otp, user.verificationOtp);
         if (!isValidOtp || Date.now() > user.verificationExpires) {
             return res.status(400).json({ success: false, error: 'Invalid or expired OTP' });
         }
@@ -95,18 +95,30 @@ exports.verifyOtp = async (req, res) => {
         user.verificationExpires = undefined; // Clear OTP expiration
         await user.save();
 
-        // Generate token and send it in the response
+        // Generate token
         const token = generateToken(user);
 
+        // Return all the registered data with the response
         res.status(200).json({
             success: true,
             message: 'Email successfully verified. You are now logged in.',
-            token: token
+            token: token,
+            userData: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                role: user.role,
+                isVerified: user.isVerified,
+                createdAt: user.createdAt,
+                image: user.image
+            }
         });
     } catch (err) {
         res.status(400).json({ success: false, error: err.message });
     }
 };
+
 
 // Login User (Email/Password)
 exports.login = async (req, res) => {
